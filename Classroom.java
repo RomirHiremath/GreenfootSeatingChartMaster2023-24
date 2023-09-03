102f59ca41f3d9d4470578df5860248959346eab
Romir Hiremath <romirhiremath@gmail.com>
2023-09-02 22:55:37 -0700

    changes by Romir Hiremath

--- Classroom.java
+++ Classroom.java
@@ -149,9 +149,13 @@ public class Classroom extends World
  // Each student needs to create their specific instance following the KilgoreTrout example.
  // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
  // and then clicking on the inspect text
-        KilgoreTrout kilgoretrout = new KilgoreTrout();
+       /* KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
-        kilgoretrout.assignSeat();
+        kilgoretrout.assignSeat();*/
+        
+        RomirHiremath romirhiremath = new RomirHiremath();
+        addObject(romirhiremath,2,3);
+        romirhiremath.assignSeat();
   
     }
     
